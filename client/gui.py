66af627211536e8b32b60d0b572fca383560b5ff
youkononame <youko@tuta.io>
2025-03-16 17:20:45 -0400

    Something or other

--- client/gui.py
+++ client/gui.py
@@ -12,14 +12,21 @@ font1 = pygame.font.Font("../assets/PixelOperator8.ttf", 20)
 font2 = pygame.font.Font("../assets/PixelOperator8.ttf", 80)
 font3 = pygame.font.Font("../assets/PixelOperator8.ttf", 50)
 
-text = "SPIN"
 padding = 10
-surf = font1.render(text, True, "black")
+scale = 1.5
 
-button1 = pygame.Rect(150, 460, font1.size(text)[0] + padding, font1.size(text)[1] + padding)
-button_next = pygame.Rect(250, 460, font1.size("NEXT")[0] + padding, font1.size("NEXT")[1] + padding)
+spin_img = pygame.image.load("../assets/SpinButton.png")
+spin_img_size = spin_img.get_size()
+spin_img = pygame.transform.scale(spin_img, (spin_img_size[0] * scale, spin_img_size[1] * scale))
+spin_img_size = spin_img.get_size()
 
-surf2 = font1.render("NEXT", True, "black")
+next_img = pygame.image.load("../assets/NextButton.png")
+next_img_size = next_img.get_size()
+next_img = pygame.transform.scale(next_img, (next_img_size[0] * scale, next_img_size[1] * scale))
+next_img_size = next_img.get_size()
+
+button_spin = pygame.Rect((screen.get_width() / 2) - spin_img_size[0] - 60, 433, spin_img_size[0] + padding, spin_img_size[1] + padding)
+button_next = pygame.Rect((screen.get_width() / 2) + next_img_size[0] - 60, 433, next_img_size[0] + padding, next_img_size[1] + padding)
 
 background_img = pygame.image.load("../assets/BackgroundSlotMachine.png")
 dash_img = pygame.image.load("../assets/ResultSlotDash.png")
@@ -27,15 +34,14 @@ dot_img = pygame.image.load("../assets/ResultSlotDot.png")
 space_img = pygame.image.load("../assets/ResultSlotSpace.png")
 slash_img = pygame.image.load("../assets/ResultSlotSlash.png")
 
-def draw_button(button, surf):
-    pygame.draw.rect(screen, (110, 110, 110), button)
-    screen.blit(surf, (button.x + (padding / 2), button.y + (padding / 2)))
+def draw_button(button, texture):
+    screen.blit(texture, (button.x + (padding / 2), button.y + (padding / 2)))
 
 letter = ""
 while True:
     screen.fill((255, 255, 255))
     
-    screen.blit(background_img, (0, 0))
+    screen.blit(background_img, (0, -25))
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -43,17 +49,17 @@ while True:
             quit()
             
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if button1.collidepoint(event.pos):
+            if button_spin.collidepoint(event.pos):
                 letter = random.choice(string.ascii_lowercase + " ")
             elif button_next.collidepoint(event.pos):
                 pass
 
-    draw_button(button1, surf)
-    draw_button(button_next, surf2)
+    draw_button(button_spin, spin_img)
+    draw_button(button_next, next_img)
     
     for i in range(4):
         mc_letter = client.letters[letter]
-        pos = (90 * i + (78 if i == 1 else 79), 257)
+        pos = (90 * i + (78 if i == 1 else 79), 232)
         if i < len(mc_letter):
             match(mc_letter[i]):
                 case "-":
