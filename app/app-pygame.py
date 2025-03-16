fd7df81e883e26eef626d90bc1ba7dc1f4a00448
TheMarkoLoncar <TheMarkoLoncar@gmail.com>
2025-03-16 13:31:44 -0400

    Update app-pygame.py

--- app/app-pygame.py
+++ app/app-pygame.py
@@ -1,7 +1,6 @@
 import pygame
 import random
 
-
 pygame.init()
 
 screen = pygame.display.set_mode((500, 500))
@@ -11,15 +10,20 @@ font = pygame.font.SysFont('MS Sans Serif', 40, bold=True)
 text = "SPIN"
 padding = 10
 surf = font.render(text, True, 'black')
-button = pygame.Rect(75, 350, font.size(text)[0] + padding, font.size(text)[1] + padding)
+button1 = pygame.Rect(75, 250, font.size(text)[0] + padding, font.size(text)[1] + padding)
+button2 = pygame.Rect(150, 250, font.size(text)[0] + padding, font.size(text)[1] + padding)
+button3 = pygame.Rect(225, 250, font.size(text)[0] + padding, font.size(text)[1] + padding)
 letter1 = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 letter2 = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 letter3 = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 font2 = pygame.font.SysFont('MS Sans Serif', 100, bold=True)
 text2 = f"{letter1} - {letter2} - {letter3}"
 surf2 = font2.render(text2, True, 'black')
-rectangle = pygame.Rect(75, 350, font.size(text2)[0] + padding, font.size(text2)[1] + padding)
 screen.fill((255, 255, 255))
+
+def draw_button(button, padding, screen, surf):
+    pygame.draw.rect(screen, (110, 110, 110), button)
+    screen.blit(surf, (button.x + (padding / 2), button.y + (padding / 2)))
 
 while True:
     for event in pygame.event.get():
@@ -27,22 +31,37 @@ while True:
             pygame.quit()
             quit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if button.collidepoint(event.pos):
+            if button1.collidepoint(event.pos):
                screen.fill((255, 255, 255))
                letter1 = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-               letter2 = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-               letter3 = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
                font2 = pygame.font.SysFont('MS Sans Serif', 100, bold=True)
                text2 = f"{letter1} - {letter2} - {letter3}"
                surf2 = font2.render(text2, True, 'black')
                screen.blit(surf2, (100, 100)) 
                pygame.display.update()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if button2.collidepoint(event.pos):
+                screen.fill((255, 255, 255))
+                letter2 = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+                font2 = pygame.font.SysFont('MS Sans Serif', 100, bold=True)
+                text2 = f"{letter1} - {letter2} - {letter3}"
+                surf2 = font2.render(text2, True, 'black')
+                screen.blit(surf2, (100, 100)) 
+                pygame.display.update()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if button3.collidepoint(event.pos):
+                screen.fill((255, 255, 255))
+                letter3 = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+                font2 = pygame.font.SysFont('MS Sans Serif', 100, bold=True)
+                text2 = f"{letter1} - {letter2} - {letter3}"
+                surf2 = font2.render(text2, True, 'black')
+                screen.blit(surf2, (100, 100)) 
+                pygame.display.update()
     
     a,b = pygame.mouse.get_pos()
-    if button.x <= a <= button.x + font.size(text)[0] + padding and button.y <= b <= button.y + font.size(text)[1] + padding:
-        pygame.draw.rect(screen, (180, 180, 180), button)
-    else:
-        pygame.draw.rect(screen, (110, 110, 110), button)
-    screen.blit(surf, (button.x + (padding / 2), button.y + (padding / 2)))
+    draw_button(button1, padding, screen, surf)
+    draw_button(button2, padding, screen, surf)
+    draw_button(button3, padding, screen, surf)
+
     
     pygame.display.update()
