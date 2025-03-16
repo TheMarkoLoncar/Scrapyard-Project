5be472aff033f1ab465bc2868a962341b1c7ce57
TheMarkoLoncar <TheMarkoLoncar@gmail.com>
2025-03-16 10:50:59 -0400

    Initializing window

---
+++ project/app.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+window = Tk()
+window.title("morse code translator")
+window.geometry("500x500")
+window.config(bg="white")
+
+#text header
+heading = Label(window,
+                text="Morse Code Translator lol",
+                font=("Arial", 24),
+                fg="black",
+                bg="white",
+                )
+#this is what makes the text appear
+heading.pack()
+
+#this is what the translator will port into
+def click():
+    pass
+
+
+window.mainloop()
